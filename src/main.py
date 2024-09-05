69cf86ad79be38eda4e841b7f300195ec158c050
Patryk Jura <you@example.com>
2024-09-05 21:08:07 +0200

    Added dataset and loading from this file

--- src/main.py
+++ src/main.py
@@ -3,9 +3,12 @@ from model import train_test_split
 from model import train_model
 from model import  check_model
 
+
 def run() -> None:
-    data_path = "D:/project/Student_Performance.csv"
+    data_path = "../data/Student_Performance-Dataset.csv"
+
     dataFrame = import_data.read_data(data_path)
+
     X, Y = clean_data.clean_data(dataFrame)
 
     x_train, x_test, y_train, y_test = train_test_split.split_dataFrame(X, Y)
